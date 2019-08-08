bdd6b9ddacd1e4736e447239ac14c2c18f83cc8a
eevabec <evan.bechtol@ericsson.com>
2019-08-08 09:31:17 -0500

    Refactor expect to assert

--- test/WeatherService.js
+++ test/WeatherService.js
@@ -1,5 +1,4 @@
 const assert = require( "chai" ).assert;
-const expect = require( "chai" ).expect;
 const mocha = require( "mocha" );
 const describe = mocha.describe;
 const it = mocha.it;
@@ -10,26 +9,32 @@ describe( "Weather Service", () => {
   const WeatherServiceInstance = new WeatherService( config.weatherApi, config.weatherApiKey );
 
   it( "Should initialize an instance of the weather service", () => {
-    expect( WeatherServiceInstance ).to.be.an.instanceOf( WeatherService );
+    assert.instanceOf( WeatherServiceInstance, WeatherService );
   } );
 
   describe( "Check methods", () => {
 
     describe( "Method getApi", () => {
       it( "Should have method", () => {
-        expect( WeatherServiceInstance ).itself.to.respondTo( "getApi" );
+        assert.isFunction( WeatherServiceInstance.getApi );
       } );
     } );
 
     describe( "Method setApi", () => {
       it( "Should have method", () => {
-        expect( WeatherServiceInstance ).itself.to.respondTo( "setApi" );
+        assert.isFunction( WeatherServiceInstance.setApi );
+      } );
+    } );
+
+    describe( "Method makeRequest", () => {
+      it( "Should have method", () => {
+        assert.isFunction( WeatherServiceInstance.makeRequest );
       } );
     } );
 
     describe( "Method getWeather", () => {
       it( "Should have method", () => {
-        expect( WeatherServiceInstance ).itself.to.respondTo( "getWeather" );
+        assert.isFunction( WeatherServiceInstance.getWeather );
       } );
 
       let weatherResponse;
@@ -38,7 +43,7 @@ describe( "Weather Service", () => {
         const latitude = "32.8998";
         const longitude = "97.0403";
         weatherResponse = await WeatherServiceInstance.getWeather( latitude, longitude );
-        assert.isObject(weatherResponse);
+        assert.isObject( weatherResponse );
       } );
 
       describe( "Response should have keys", () => {
