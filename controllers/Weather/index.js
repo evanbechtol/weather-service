e231353460d16462ec4e1f4e0cbe7c27f5cba45c
eevabec <evan.bechtol@ericsson.com>
2019-08-08 10:55:32 -0500

    Weather Controller
    - Finish method `getForecast`

--- controllers/Weather/index.js
+++ controllers/Weather/index.js
@@ -7,8 +7,18 @@ module.exports = {
   getWeather
 };
 
-function getForecast ( req, res ) {
+async function getForecast ( req, res ) {
+  try {
+    const ForecastResponse = await WeatherServiceInstance.getForecast( req.weatherParams );
 
+    if (!ForecastResponse) {
+      return res.status( 404 ).send();
+    }
+
+    return res.status( ForecastResponse.cod ).send( ForecastResponse );
+  } catch( err ) {
+    return res.status( err.cod ).send( err );
+  }
 }
 
 function getWeather ( req, res ) {
