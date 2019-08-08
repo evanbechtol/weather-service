657dd333934f4f49db085796cc8148fb72b13155
eevabec <evan.bechtol@ericsson.com>
2019-08-08 10:29:42 -0500

    Remove old test code from index.js

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
 // Create express instance to setup API
 const ExpressLoader = require( "./loaders/Express" );
 new ExpressLoader();
-
-
-async function main() {
-  const config = require( "./config" );
-  const WeatherService = require( "./services/WeatherService" );
-  const WeatherServiceInstance = new WeatherService( config.weatherApi, config.weatherApiKey );
-
-  const latitude = "32.8998";
-  const longitude = "-96.80667";
-
-  const response = await WeatherServiceInstance.getWeather(latitude, longitude);
-  console.log( response );
-
-}
-
-main();
