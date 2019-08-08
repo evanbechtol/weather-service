ae59072ab65372831030a65488a5c6ee8139210d
eevabec <evan.bechtol@ericsson.com>
2019-08-08 10:34:15 -0500

    Add weather controller to weather routes

--- routes/weather/index.js
+++ routes/weather/index.js
@@ -1,5 +1,6 @@
 const express = require( 'express' );
 const router = express.Router();
+const WeatherController = require( "../../controllers/Weather" );
 
 router.get( '/weather', ( req, res ) => {
   res.send( { message : 'Hello from weather!' } );
